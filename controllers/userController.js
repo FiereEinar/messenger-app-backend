d1dbf5f673e60f0f43f063ca7f9b46468c5e2d8b
FiereEinar <nickxylanmelloria@gmail.com>
2024-07-03 06:17:54 +0800

    feature: cover photo update

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,7 @@ exports.user_info_get = asyncHandler(async (req, res) => {
  */
 exports.user_update = asyncHandler(async (req, res) => {
   const { userID } = req.params;
-  const { firstname, lastname, username } = req.body;
+  const { firstname, lastname, username, bio } = req.body;
 
   // check if the user exists
   const user = await User.findById(userID);
@@ -45,8 +45,8 @@ exports.user_update = asyncHandler(async (req, res) => {
     return res.json(new Response(false, null, 'User not found', null));
   }
 
-  let profileURL = '';
-  let profilePublicID = '';
+  let profileURL = user.profile.url;
+  let profilePublicID = user.profile.publicID;
 
   // if there is an uploaded image, update the user profile
   if (req.file) {
@@ -67,6 +67,7 @@ exports.user_update = asyncHandler(async (req, res) => {
     firstname: firstname,
     lastname: lastname,
     username: username,
+    bio: bio,
     profile: {
       url: profileURL,
       publicID: profilePublicID,
@@ -76,4 +77,46 @@ exports.user_update = asyncHandler(async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(userID, update, { new: true }).exec();
 
   return res.json(new Response(true, updatedUser, 'User updated', null));
+});
+
+/**
+ * PUT - UPDATE USER COVER PHOTO
+ */
+exports.user_cover_update = asyncHandler(async (req, res) => {
+  const { userID } = req.params;
+
+  // check if the user exists
+  const user = await User.findById(userID);
+  if (!user) {
+    return res.json(new Response(false, null, 'User not found', null));
+  }
+
+  let coverURL = user.cover.url;
+  let coverPublicID = user.cover.publicID;
+
+  // if there is an uploaded image, update the user cover
+  if (req.file) {
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+    coverURL = result.secure_url;
+    coverPublicID = result.public_id;
+
+    // if there was a previous cover, delete that from the cloud
+    if (user.cover.publicID.length > 0) {
+      await cloudinary.uploader.destroy(user.cover.publicID);
+    }
+
+    await fs.unlink(req.file.path);
+  }
+
+  const update = {
+    cover: {
+      url: coverURL,
+      publicID: coverPublicID,
+    }
+  };
+
+  const updatedUser = await User.findByIdAndUpdate(userID, update, { new: true }).exec();
+
+  return res.json(new Response(true, updatedUser, 'User cover photo updated', null));
 });
--- routes/user.js
+++ routes/user.js
@@ -7,10 +7,12 @@ const {
   users_get,
   user_info_get,
   user_update,
+  user_cover_update,
 } = require('../controllers/userController');
 
 router.get('/', passport.authenticate('jwt', { session: false }), users_get);
 router.get('/:userID', passport.authenticate('jwt', { session: false }), user_info_get);
+router.put('/:userID/cover', passport.authenticate('jwt', { session: false }), upload.single('coverPhoto'), user_cover_update);
 router.put('/:userID', passport.authenticate('jwt', { session: false }), upload.single('profileImg'), user_update);
 
 module.exports = router;
